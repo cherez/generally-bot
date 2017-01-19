f193603df510b8d60ba3aeb507c1f7f22ccc2ccc
Steven Wallace <snwallace@gmail.com>
2017-01-19 12:37:00 -0600

    Added debug flag to config.

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ import bot
 
 if __name__ == '__main__':
     import logging
-    logging.basicConfig()
-    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
     config = yaml.load(open('config'))
+    if(config.get('debug')):
+        logging.basicConfig()
+        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
     bot.Bot(config).start()
     bot = bot.Bot(config['chan'], config['name'], config['pass'], config['server'])
     bot.start()
