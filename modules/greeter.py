6af27fd10e56b7a2f24a14cbf67f71bb4ffd7d10
Steven Wallace <snwallace@gmail.com>
2017-06-14 16:42:21 -0500

    Migrated modules to modules-available.
    Symlink the modules you want to enable.

--- modules/greeter.py
+++ modules/greeter.py
@@ -1,11 +1,1 @@
-from handlers import handle
-from config import config
-
-greeting = config.get('greeting')
-
-#don't greet if no greeting is set
-if greeting:
-    @handle('new-users')
-    def greet(connection, event):
-        for user in event.arguments:
-            connection.say(greeting.format(name=user.name))
+../modules-available/greeter.py
